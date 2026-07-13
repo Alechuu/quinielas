47aea1471fb31be35d923a9b51edba6ca6fcc4b5
Alechu <alejo.f@sancrisoft.com>
2026-07-13 19:58:47 -0300

    Fix for cabezas function 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+const tesseractRuntimeDeps = [
+  "./node_modules/tesseract.js/**/*",
+  "./node_modules/tesseract.js-core/**/*",
+  "./node_modules/bmp-js/**/*",
+  "./node_modules/idb-keyval/**/*",
+  "./node_modules/wasm-feature-detect/**/*",
+  "./node_modules/regenerator-runtime/**/*",
+  "./node_modules/is-url/**/*",
+  "./node_modules/zlibjs/**/*",
+  "./node_modules/node-fetch/**/*",
+];
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -9,21 +21,11 @@ const nextConfig = {
   serverExternalPackages: [
     "tesseract.js",
     "tesseract.js-core",
+    "bmp-js",
     "wasm-feature-detect",
   ],
   outputFileTracingIncludes: {
-    "/api/cabezas": [
-      "./node_modules/tesseract.js/src/worker-script/**/*",
-      "./node_modules/tesseract.js/src/worker/**/*",
-      "./node_modules/tesseract.js/src/utils/**/*",
-      "./node_modules/tesseract.js/src/constants/**/*",
-      "./node_modules/tesseract.js-core/**/*",
-      "./node_modules/wasm-feature-detect/**/*",
-      "./node_modules/regenerator-runtime/**/*",
-      "./node_modules/is-url/**/*",
-      "./node_modules/zlibjs/**/*",
-      "./node_modules/node-fetch/**/*",
-    ],
+    "/api/cabezas": tesseractRuntimeDeps,
   },
 };
 
